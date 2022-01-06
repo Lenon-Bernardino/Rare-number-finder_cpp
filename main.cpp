83b78838a2bc16ba5ed082d66ea4377fb1f3c6d4
JUKjacker <80601981+MrWitzbold@users.noreply.github.com>
2022-01-06 12:31:05 -0300

    More efficient skipping certain numbers

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ long long int first_digit(long long int n)
 
 int main() 
 {
-    long long int i = 281089082;
+    long long int i = 0;
     while(true) 
     {
         long long int reversed_number = get_reversed_number(i);
@@ -49,11 +49,93 @@ int main()
                 outfile << i << "\n";
             }
         }
-        i += 1;
+        // i += 1;
         if((first_digit(i) % 2) != 0)
         {
             long long int digits = to_string(i).length()-1;
             i += pow(10, digits);
         }
+        // can end in 0, 2, 3, 5, 7, and 8
+        // 0 + 2
+        // 1 + 1
+        // 2 + 1
+        // 3 + 2
+        // 4 + 1
+        // 5 + 2
+        // 6 + 1
+        // 7 + 1
+        // 8 + 2
+        // 9 + 1
+
+        if((i % 10) == 0)
+        {
+            i += 2;
+        }
+        else
+        {
+            if((i % 10) == 1)
+            {
+                i += 1;
+            }
+            else
+            {
+                if((i % 10) == 2)
+                {
+                    i += 1;
+                }
+                else
+                {
+                    if((i % 10) == 3)
+                    {
+                        i += 2;
+                    }
+                    else
+                    {
+                        if((i % 10) == 4)
+                        {
+                            i += 1;
+                        }
+                        else
+                        {
+                            if((i % 10) == 5)
+                            {
+                                i += 2;
+                            }
+                            else
+                            {
+                                if((i % 10) == 6)
+                                {
+                                    i += 1;
+                                }
+                                else
+                                {
+                                    if((i % 10) == 7)
+                                    {
+                                        i += 1;
+                                    }
+                                    else
+                                    {
+                                        if((i % 10) == 8)
+                                        {
+                                            i += 2;
+                                        }
+                                        else
+                                        {
+                                            if((i % 10) == 9)
+                                            {
+                                                i += 1;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+
+        cout << "i: " << i << "\n";
     }
 }
