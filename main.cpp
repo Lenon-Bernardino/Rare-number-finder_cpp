bc4bc5d2eabf97a4c95262f040720d4b39b79bd3
Lenon Saturnino Bernardino <70743202+Lenon-Bernardino@users.noreply.github.com>
2022-09-18 02:32:40 -0300

    skips a bunch of numbers

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <Windows.h>
+#include <math.h>
 
 std::string reverse_string(std::string str)
 {
@@ -23,22 +25,39 @@ int main()
 	std::string reversed_num_str = "";
 	
 	BigInt subtraction = initial;
+	int first_digit;
+	bool valid_first_digit;
+	int last_digit;
 
 	while(true)
 	{
-		//std::cout << "num = " << num << "\n";
+		//Sleep(1000);
+
+		num += 1;
+		num_str = num.to_string();
+		first_digit = (int)num_str[0]-48;
+		valid_first_digit = first_digit % 2 == 0;
+
+		if(valid_first_digit == false)
+		{
+			//std::cout << "\nAdding: " << pow(10, num_str.size()-1);
+			num += pow(10, num_str.size()-1);
+			num_str = num.to_string();
+			//std::cout << "\ncorrected to " << num << "\n";
+		}
+		last_digit = std::stoi((num % 10).to_string());
+		
+		std::cout << "num = " << num << "\n";
 		//std::cout << "num2 = " << num2 << "\n";
 		//std::cout << "sum = " << num+num2 << "\n";
 		//std::cout << "result = " << result << "\n";
 		//std::cout << "==================================================================";
 
-		num += 1;
-		num_str = num.to_string();
 		reversed_num_str = reverse_string(num_str);
 		num2 = reversed_num_str;
 		
 		result = sqrt(num+num2);
-		if(result*result == num+num2 && num_str != reversed_num_str)
+		if(result*result == num+num2 && num_str != reversed_num_str && num > num2)
 		{
 			if(num >= num2)
 			{
