1b7a3cbdb16ef5b4600fed4ef1c8040ae2a00973
Lenon Saturnino Bernardino <70743202+Lenon-Bernardino@users.noreply.github.com>
2022-09-18 00:46:34 -0300

    Use bigint library

--- main.cpp
+++ main.cpp
@@ -1,141 +1,58 @@
+#include "BigInt.hpp"
+#include <stdio.h>
 #include <iostream>
-#include <cmath>
-#include <string.h>
-#include <Windows.h>
-#include <fstream>
+#include <string>
 
-using namespace std;
-
-long long int get_reversed_number(long long int number)
+std::string reverse_string(std::string str)
 {
-    long long int reversed_number = 0;
-    while (number > 0)
+    std::string new_string = "";
+    for(int i = str.length()-1; i <= str.length()-1; i--)
     {
-        reversed_number = reversed_number * 10 + number % 10;
-        number = number / 10;
+        new_string += str.at(i);
     }
-    return reversed_number;
+    return new_string;
 }
 
-long long int first_digit(long long int n)
+int main()
 {
-    // Find total number of digits - 1
-    long long int digits = (long long int)log10(n);
+	std::string initial = "0";
+	BigInt num = initial;
+	BigInt num2 = initial;
+	BigInt result = initial;
+	std::string num_str = "";
+	std::string reversed_num_str = "";
+	
+	BigInt subtraction = initial;
 
-    // Find first digit
-    n = (long long int)(n / pow(10, digits));
+	while(true)
+	{
+		//std::cout << "num = " << num << "\n";
+		//std::cout << "num2 = " << num2 << "\n";
+		//std::cout << "sum = " << num+num2 << "\n";
+		//std::cout << "result = " << result << "\n";
+		//std::cout << "==================================================================";
 
-    // Return first digit
-    return n;
-}
-
-int main() 
-{
-    long long int i = 0;
-    while(true) 
-    {
-        long long int reversed_number = get_reversed_number(i);
-        if(reversed_number != i)
-        {
-            double root = sqrt(reversed_number + i);
-            bool perfect_square_addition = ((floor(root)-root) == 0);
-            bool odd_digits = ((to_string(i).length() % 2) != 0);
-            bool perfect_square_subtraction = (floor(sqrt(abs(i - reversed_number))) - sqrt(abs(i - reversed_number))) == 0;
-            if(perfect_square_addition && perfect_square_subtraction)
-            {
-                cout << i << "\n";
-                std::ofstream outfile;
-                outfile.open("rare_numbers.txt", std::ios_base::app);
-                outfile << i << "\n";
-            }
-        }
-        // i += 1;
-        if((first_digit(i) % 2) != 0)
-        {
-            long long int digits = to_string(i).length()-1;
-            i += pow(10, digits);
-        }
-        // can end in 0, 2, 3, 5, 7, and 8
-        // 0 + 2
-        // 1 + 1
-        // 2 + 1
-        // 3 + 2
-        // 4 + 1
-        // 5 + 2
-        // 6 + 1
-        // 7 + 1
-        // 8 + 2
-        // 9 + 1
-
-        if((i % 10) == 0)
-        {
-            i += 2;
-        }
-        else
-        {
-            if((i % 10) == 1)
-            {
-                i += 1;
-            }
-            else
-            {
-                if((i % 10) == 2)
-                {
-                    i += 1;
-                }
-                else
-                {
-                    if((i % 10) == 3)
-                    {
-                        i += 2;
-                    }
-                    else
-                    {
-                        if((i % 10) == 4)
-                        {
-                            i += 1;
-                        }
-                        else
-                        {
-                            if((i % 10) == 5)
-                            {
-                                i += 2;
-                            }
-                            else
-                            {
-                                if((i % 10) == 6)
-                                {
-                                    i += 1;
-                                }
-                                else
-                                {
-                                    if((i % 10) == 7)
-                                    {
-                                        i += 1;
-                                    }
-                                    else
-                                    {
-                                        if((i % 10) == 8)
-                                        {
-                                            i += 2;
-                                        }
-                                        else
-                                        {
-                                            if((i % 10) == 9)
-                                            {
-                                                i += 1;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-
-        cout << "i: " << i << "\n";
-    }
+		num += 1;
+		num_str = num.to_string();
+		reversed_num_str = reverse_string(num_str);
+		num2 = reversed_num_str;
+		
+		result = sqrt(num+num2);
+		if(result*result == num+num2 && num_str != reversed_num_str)
+		{
+			if(num >= num2)
+			{
+				subtraction = num-num2;
+			}
+			else
+			{
+				subtraction = num2-num;
+			}
+			result = sqrt(subtraction);
+			if(result*result == subtraction)
+			{
+				std::cout << num << "\n";
+			}
+		}
+	}
 }
