834fa3110eb8c940ca6b39eda6436dc4186ab523
Lenon Saturnino Bernardino <70743202+Lenon-Bernardino@users.noreply.github.com>
2022-09-18 16:04:26 -0300

    Optimizations based on the properties of rare numbers
    If A = 4 then Q = 0 and B - P = positive or negative Even digit i.e. -8, -6, -4, -2, 0, 2, 4, 6, 8.
    If A = 6 then Q = 0 or 5 and B - P = positive or negative Odd digit i.e. -9, -7, -5, -3, -1, 1, 3, 5, 7, 9.
    If A = 8 then Q = 2, 3, 7 or 8; if Q = 2 then B + P = 9, if Q = 3 then B - P = 7 for B > P and B - P = -3 for B < P and B can never be equal to P, if Q = 7 then B + P = 11 for B >1 and B + P =1 for B < 1, if Q = 8 then B = P.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,29 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <Windows.h>
 #include <math.h>
+#include <vector>
+#include <stdlib.h>
+
+std::string display_vector(std::vector<std::string> thing)
+{
+    std::string display = "{";
+    std::string s;
+    for(int i = 0; i < thing.size(); i++)
+    {
+        s = thing.at(i);
+        if(i == 0)
+        {
+            display += s;
+        }
+        else
+        {
+            display += "," + s;
+        }
+    }
+    display += "}";
+    return display;
+}
 
 std::string reverse_string(std::string str)
 {
@@ -17,17 +38,26 @@ std::string reverse_string(std::string str)
 
 int main()
 {
+	std::string num_start = "0"; // 89650295750128200205698
 	std::string initial = "0";
-	BigInt num = initial;
+	BigInt num = num_start;
 	BigInt num2 = initial;
 	BigInt result = initial;
 	std::string num_str = "";
 	std::string reversed_num_str = "";
 	
 	BigInt subtraction = initial;
-	int first_digit;
 	bool valid_first_digit;
-	int last_digit;
+	int A_digit;
+	int B_digit;
+	int C_digit;
+	int D_digit;
+	int M_digit;
+	int N_digit;
+	int P_digit;
+	int Q_digit;
+
+	std::vector<std::string> found_numbers;
 
 	while(true)
 	{
@@ -35,19 +65,101 @@ int main()
 
 		num += 1;
 		num_str = num.to_string();
-		first_digit = (int)num_str[0]-48;
-		valid_first_digit = first_digit % 2 == 0;
+		A_digit = (int)num_str[0]-48;
+		valid_first_digit = A_digit % 2 == 0;
 
-		if(valid_first_digit == false)
+		if(valid_first_digit == false) // Skips if the first number is odd
 		{
 			//std::cout << "\nAdding: " << pow(10, num_str.size()-1);
 			num += pow(10, num_str.size()-1);
 			num_str = num.to_string();
 			//std::cout << "\ncorrected to " << num << "\n";
 		}
-		last_digit = std::stoi((num % 10).to_string());
+		//Q_digit = std::stoi((num % 10).to_string());
 		
-		std::cout << "num = " << num << "\n";
+		// Implementing some properties with the first 4 and last 4 numbers
+		if(num_str.size() >= 4)
+		{
+			A_digit = (int)num_str[0]-48;
+			B_digit = (int)num_str[1]-48;
+			P_digit = (int)num_str[num_str.size()-2]-48;
+			Q_digit = std::stoi((num % 10).to_string());
+			
+			if(A_digit == 4)
+			{
+				if(Q_digit != 0)
+				{
+					continue;
+				}
+				if(abs(B_digit-P_digit) % 2 != 0)
+				{
+					continue;
+				}
+			}
+			if(A_digit == 6)
+			{
+				if(Q_digit != 0 && Q_digit != 5)
+				{
+					continue;
+				}
+				if(abs(B_digit-P_digit) % 2 == 0)
+				{
+					continue;
+				}
+			}
+			if(A_digit == 8)
+			{
+				if(Q_digit != 2 && Q_digit != 3 && Q_digit != 7 && Q_digit != 8)
+				{
+					continue;
+				}
+				if(Q_digit == 2)
+				{
+					if(B_digit + P_digit != 9)
+					{
+						continue;
+					}
+				}
+				if(Q_digit == 3)
+				{
+					if(B_digit - P_digit != 7 && B_digit - P_digit != -3)
+					{
+						continue;
+					}
+				}
+				if(Q_digit == 7)
+				{
+					if(B_digit > 1)
+					{
+						if(B_digit + P_digit != 11)
+						{
+							continue;
+						}
+					}
+					if(B_digit < 1)
+					{
+						if(B_digit + P_digit != 1)
+						{
+							continue;
+						}
+					}
+				}
+				if(Q_digit == 8)
+				{
+					if(B_digit != Q_digit)
+					{
+						continue;
+					}
+				}
+			}
+		}
+		
+		// From here on it's logging and slow stuff that needs to be avoided.
+		std::cout << "\n==========================================";
+		std::cout << "\nnum = " << num << "\n";
+		std::cout << "Found numbers: " << display_vector(found_numbers);
+		std::cout << "\n==========================================";
+		
 		//std::cout << "num2 = " << num2 << "\n";
 		//std::cout << "sum = " << num+num2 << "\n";
 		//std::cout << "result = " << result << "\n";
@@ -70,7 +182,7 @@ int main()
 			result = sqrt(subtraction);
 			if(result*result == subtraction)
 			{
-				std::cout << num << "\n";
+				found_numbers.push_back(num.to_string());
 			}
 		}
 	}
